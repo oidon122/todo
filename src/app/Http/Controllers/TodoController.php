919d76442e4ac5650e7e4a6da09b58f5069debd0
oidon <co.kohaku.sh@gmail.com>
2024-07-30 04:14:38 +1000

    セッションとバリデーション

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
 use Illuminate\Http\Request;
 
@@ -14,11 +15,11 @@ class TodoController extends Controller
         return view('index', compact('todos'));
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $todo = $request->only(['content']);
         Todo::create($todo);
 
-        return redirect('/');
+        return redirect('/')->with('message', 'Todoを作成しました');
     }
 }
